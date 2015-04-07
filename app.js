5e226d2b5fec826d26eff9e271fca867d2011a10
Matthieu Moquet <matthieu@moquet.net>
2015-04-07 13:38:53 +0200

    Fix issues with undefined attr

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var map = L.mapbox.map('map', 'examples.map-i86nkdio', {
 // Stupid routing
 var mapId = location.hash.replace(/^#/, '');
 if (!mapId) {
-  mapId = (Math.random() + 1).toString(36).substring(7);
+  mapId = (Math.random() + 1).toString(36).substring(2, 12);
   location.hash = mapId;
 }
 
@@ -37,46 +37,76 @@ var firebase = new Firebase('https://' + config.firebase + '.firebaseio.com/');
 var markersRef = firebase.child('maps/' + mapId);
 var markers = {};
 
+function addPoint(uuid, position) {
+  var marker = L.marker([position.coords.latitude, position.coords.longitude], {
+    // zIndexOffset: (uuid === myUuid ? 1000 : 0),
+    icon: L.mapbox.marker.icon({
+      'marker-size': 'large',
+      'marker-color': (uuid === myUuid ? '#2196f3' : '#ff9800')
+    })
+  })
+  marker.addTo(map)
+
+  markers[uuid] = marker;
+}
+
+function removePoint(uuid) {
+  map.removeLayer(markers[uuid])
+  //markers[uuid] = null
+}
+
+function updatePoint(uuid, position) {
+  var marker = markers[uuid]
+  marker.setLatLng([position.coords.latitude, position.coords.longitude])
+}
+
+function putPoint(uuid, position) {
+  if (markers[uuid])
+    updatePoint(uuid, position)
+  else
+    addPoint(uuid, position)
+}
+
 var watchPositionId;
 map.on('ready', function() {
-  watchPositionId = navigator.geolocation.watchPosition(function(position) {
+  function successCoords(position) {
+    if (!position.coords) return
+
     markersRef.child(myUuid).set({
-      coords: position.coords,
+      coords: {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
       timestamp: Math.floor(Date.now() / 1000)
     })
 
     map.panTo([position.coords.latitude, position.coords.longitude])
-  });
+  }
+
+  function errorCoords() {
+    console.log('Unable to get current position')
+  }
+
+  watchPositionId = navigator.geolocation.watchPosition(successCoords, errorCoords);
 
   markersRef.on('child_added', function(childSnapshot) {
     var uuid = childSnapshot.key()
     var position = childSnapshot.val()
 
-    var marker = L.marker([position.coords.latitude, position.coords.longitude], {
-      // zIndexOffset: (uuid === myUuid ? 1000 : 0),
-      icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-color': (uuid === myUuid ? '#2196f3' : '#ff9800')
-      })
-    })
-    marker.addTo(map)
-
-    markers[uuid] = marker;
+    addPoint(uuid, position)
   })
 
   markersRef.on('child_changed', function(childSnapshot) {
     var uuid = childSnapshot.key()
     var position = childSnapshot.val()
 
-    var marker = markers[uuid]
-    marker.setLatLng([position.coords.latitude, position.coords.longitude])
+    putPoint(uuid, position)
   })
 
   markersRef.on('child_removed', function(oldChildSnapshot) {
     var uuid = oldChildSnapshot.key()
 
-    map.removeLayer(markers[uuid])
-    markers[uuid] = null
+    removePoint(uuid)
   })
 });
 
@@ -87,8 +117,7 @@ setInterval(function() {
       var uuid = childSnapshot.key()
       var now = Math.floor(Date.now() / 1000)
       if (childSnapshot.val().timestamp < now - 60 * 30) {
-        var marker = markers[uuid]
-        map.removeLayer(markers[uuid])
+        removePoint(uuid)
         markersRef.child(myUuid).set(null)
         //markers[uuid] = null
       }
