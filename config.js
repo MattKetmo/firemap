f939a8af538e90c976f6578b470136600c118b63
Matthieu Moquet <matthieu@moquet.net>
2015-04-16 17:41:01 +0200

    Build GH Pages
    Add analytics tracking

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  firebase: 'locatme',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoibWF0dGtldG1vIiwiYSI6IjZtQ2ZDRFUifQ.cRea26bUW1FA-hxTL5fDzA',
+      mapId: 'mattketmo.locpi8o0',
+  },
+  analytics: 'UA-23657373-6',
+}
